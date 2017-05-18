bd925b9a268cbb36663ba42741bc67436180d5b2
Eddy Kim <eddykim@186590d2f343.sea.amazon.com>
2017-05-18 15:03:47 -0700

    Initial Commit

---
+++ Black Hat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Black Hat
+//
+//  Created by Kim, Eddy on 5/18/17.
+//  Copyright © 2017 Mihky's Laboratory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
